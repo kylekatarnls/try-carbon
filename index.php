c4960ddfeab42b10a15be3faaefc14d567589379
KyleK <kylekatarnls@gmail.com>
2018-07-08 10:36:15 +0200

    Allow to configure initial width

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+$width = isset($_GET['width']) ? $_GET['width'] : 70;
 $engine = isset($_GET['engine']) ? $_GET['engine'] : 'carbon';
 $inline = isset($_GET['inline']) ? boolval(intval($_GET['inline'])) : true;
 $inputLanguage = isset($_GET['language']) ? $_GET['language'] : 'php';
@@ -80,12 +81,12 @@ include_once __DIR__ . '/allow-csrf.php';
     }
     #input {
         left: 20px;
-        right: calc(50% + 10px);
+        right: calc(<?php echo $width; ?>% + 10px);
     }
     #options {
         bottom: auto;
         height: 0;
-        right: calc(50% + 10px);
+        right: calc(<?php echo 100 - $width; ?>% + 10px);
         overflow: visible;
         z-index: 8;
         display: none;
@@ -123,7 +124,7 @@ include_once __DIR__ . '/allow-csrf.php';
     #preview,
     #output {
         right: 20px;
-        left: calc(50% + 10px);
+        left: calc(<?php echo $width; ?>% + 10px);
     }
     #preview {
         display: none;
@@ -136,7 +137,7 @@ include_once __DIR__ . '/allow-csrf.php';
         overflow: auto;
     }
     #h-resize {
-        left: calc(50% - 7px);
+        left: calc(<?php echo $width; ?>% - 7px);
         width: 14px;
         cursor: col-resize;
     }
@@ -158,7 +159,7 @@ include_once __DIR__ . '/allow-csrf.php';
             top: 0;
             height: 100%;
             left: 0;
-            right: calc(50% + 7px);
+            right: calc(<?php echo $width; ?>% + 7px);
         }
         #h-resize {
             top: 0;
@@ -169,7 +170,7 @@ include_once __DIR__ . '/allow-csrf.php';
             top: 0;
             bottom: 0;
             right: 0;
-            left: calc(50% + 7px);
+            left: calc(<?php echo 100 - $width; ?>% + 7px);
         }
         #right-buttons a,
         #options a {
@@ -180,7 +181,7 @@ include_once __DIR__ . '/allow-csrf.php';
             display: block;
             top: 0;
             height: 0;
-            right: calc(50% + 7px);
+            right: calc(<?php echo $width; ?>% + 7px);
         }
     <?php } ?>
 </style>
