0365750343171c8eedfc6b6dea921a71a7c00010
KyleKatarn <kylekatarnls@gmail.com>
2018-07-13 11:31:59 +0200

    Import all classes of the Carbon namespace

--- api/carbon.php
+++ api/carbon.php
@@ -15,7 +15,7 @@ function exception_error_handler($severity, $message, $file, $line) {
     }
     throw new ErrorException($message, 0, $severity, $file, $line);
 }
-set_error_handler("exception_error_handler");
+set_error_handler('exception_error_handler');
 
 $autoload = __DIR__ . '/../var/engines/carbon/' . $_POST['version'] . '/vendor/autoload.php';
 
@@ -28,8 +28,8 @@ include_once __DIR__ . '/../allow-csrf.php';
 require_once $autoload;
 
 try {
-    eval('use Carbon\Carbon;' . $_POST['input']);
-} catch (\Exception $e) {
+    eval('use Carbon\Carbon; use Carbon\CarbonInterval; use Carbon\CarbonInterface; use Carbon\CarbonImmutable; use Carbon\CarbonPeriod; use Carbon\CarbonTimeZone; use Carbon\Translator;' . $_POST['input']);
+} catch (\Throwable $e) {
     $message = trim($e->getMessage());
     echo 'Error' . (substr($message, 0, 1) === '('
         ? preg_replace('/^\((\d+)\)(\s*:)?/', ' line $1:', $message)
