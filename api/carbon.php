825510c8508fcac63f336914df2e763220d55be7
KyleKatarn <kylekatarnls@gmail.com>
2019-02-21 15:31:03 +0100

    Cleanup

--- api/carbon.php
+++ api/carbon.php
@@ -14,9 +14,9 @@ header('Content-type: text/plain');
 
 function exception_error_handler($severity, $message, $file, $line) {
     if (!(error_reporting() & $severity)) {
-        // Ce code d'erreur n'est pas inclu dans error_reporting
         return;
     }
+
     throw new ErrorException($message, 0, $severity, $file, $line);
 }
 set_error_handler('exception_error_handler');
