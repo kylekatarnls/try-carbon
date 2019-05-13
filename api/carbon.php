1e76e3970a2d0f8aac62acd5ca6f02551de2a48e
KyleKatarn <kylekatarnls@gmail.com>
2019-05-13 16:32:26 +0200

    Lock before execute

--- api/carbon.php
+++ api/carbon.php
@@ -6,10 +6,6 @@ if (function_exists('xdebug_disable')) {
 
 ini_set('html_errors', false);
 
-if (is_writeable(__FILE__)) {
-    //shell_exec('chmod -R 0555 ..');
-}
-
 header('Content-type: text/plain');
 
 function exception_error_handler($severity, $message, $file, $line) {
@@ -24,11 +20,12 @@ set_error_handler('exception_error_handler');
 $autoload = __DIR__ . '/../var/engines/carbon/' . $_POST['version'] . '/vendor/autoload.php';
 
 if (!file_exists($autoload)) {
-    if (substr($_POST['input'], 0, 6) === 'debug:') {
-        eval(substr($_POST['input'], 6));
-    }
     echo 'Update in progress, please retry in few minutes.';
     exit;
+}
+
+if (is_writeable(__FILE__)) {
+    shell_exec('chmod -R 0555 ..');
 }
 
 include_once __DIR__ . '/../allow-csrf.php';
