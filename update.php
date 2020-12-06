f4b8512afa50a5ec45a2b7889266bee559bfb200
KyleKatarn <kylekatarnls@gmail.com>
2020-12-06 12:54:07 +0100

    Add 3.x-dev version

--- update.php
+++ update.php
@@ -38,8 +38,6 @@ $addons = [
     'kylekatarnls/carbonite' => ['2.24.0'],
 ];
 
-$devMasterAlias = '2.99999.99999';
-
 list($majorMinimum, $minorMinimum) = explode('.', $minimumVersion);
 
 header('Content-type: text/plain; charset=UTF-8');
@@ -77,70 +75,93 @@ foreach ($enginesRepositories as $repository => $url) {
     needDirectory($directory);
     $versionCache = $cacheDirectory . DIRECTORY_SEPARATOR . $repository . '-tags.json';
     $versionFile = $versionCache;
+
     if (!file_exists($versionCache) || time() - filemtime($versionCache) > 3600) {
         $list = array();
+
         for ($i = 1; true; $i++) {
             $items = json_decode(file_get_contents(
                 $apiHost . 'repos/' . $url . '/tags?page=' . $i,
                 false,
                 $apiContext
             ));
+
             if (!is_array($items)) {
                 $items = json_decode(file_get_contents(
                     __DIR__ . '/fallback/' . $repository . '-tags.json'
                 ));
             }
+
             $list = array_merge($list, $items);
+
             if (count($items) < 30) {
                 break;
             }
         }
+
         file_put_contents($versionCache, json_encode($list));
     }
+
     $touched = false;
     $tags = json_decode(file_get_contents($versionCache));
     usort($tags, function ($a, $b) {
         return version_compare($b->name, $a->name);
     });
-    array_unshift($tags, (object) [
-       'name' => 'master (next version)',
-    ]);
+
+    array_unshift(
+        $tags,
+        (object) [
+            'name' => '2.x-dev',
+        ],
+        (object) [
+            'name' => '3.x-dev',
+        ]
+    );
+
     $minorTags = [];
+
     foreach ($tags as $tag) {
         list($major, $minor, $patch) = explode('.', $tag->name . '..');
         $minorTag = "$major.$minor";
+
         if (isAvailable($major, $minor, $patch, $minorTag)  && !isset($minorTags[$minorTag])) {
             $minorTags[$minorTag] = $tag;
         }
     }
+
     foreach ($minorTags as $tag) {
         echo "Load $url {$tag->name}\n";
-        $isMaster = (strpos($tag->name, 'master') !== false);
-        $shortName = $isMaster ? 'master' : $tag->name;
-        $optionsHtml .= '<option value="' . $shortName . '">' . $tag->name . '</option>';
-        $versionDirectory = $directory . DIRECTORY_SEPARATOR . $shortName;
+        $optionsHtml .= '<option value="' . $tag->name . '">' . $tag->name . '</option>';
+        $versionDirectory = $directory . DIRECTORY_SEPARATOR . $tag->name;
+
         if (needDirectory($versionDirectory) || !file_exists($versionDirectory . '/vendor/autoload.php')) {
             $touched = true;
             $composerJson = [
                 'require' => [
-                    'nesbot/carbon' => $isMaster ? "dev-master as $devMasterAlias" : $tag->name,
+                    'nesbot/carbon' => $tag->name,
                 ],
             ];
-            $currentVersion = $isMaster ? $devMasterAlias : $tag->name;
+            $currentVersion = str_replace('.x-dev', '.99999.99999', $tag->name);
+
             foreach ($addons as $name => $versions) {
                 list($min, $max) = array_pad($versions, 2, null);
+
                 if ($min && version_compare($currentVersion, $min, '<')) {
                     continue;
                 }
+
                 if ($max && version_compare($currentVersion, $max, '>')) {
                     continue;
                 }
-                $composerJson['require'][$name] = 'dev-master';
+
+                $composerJson['require'][$name] = '2.x-dev';
             }
+
             file_put_contents($versionDirectory.'/composer.json', json_encode($composerJson));
             echo shell_exec("cd $versionDirectory && composer update --optimize-autoloader --no-dev --ignore-platform-reqs")."\n\n";
         }
     }
+
     if ($touched) {
         file_put_contents($cacheDirectory . DIRECTORY_SEPARATOR . $repository . '-versions-options.html', $optionsHtml);
     }
