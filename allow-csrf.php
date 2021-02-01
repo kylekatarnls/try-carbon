0f194beb81da0153ed8dff91fbbd2a65af7fa0c0
KyleKatarn <kylekatarnls@gmail.com>
2021-02-01 22:42:30 +0100

    Set origin for doc

--- allow-csrf.php
+++ allow-csrf.php
@@ -2,5 +2,5 @@
 
 header('Access-Control-Allow-Headers: *');
 header('Access-Control-Allow-Methods: *');
-header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Origin: ' . ($_SERVER['HTTP_ORIGIN'] === 'https://carbon.nesbot.com' ? 'https://carbon.nesbot.com' : '*'));
 header('X-XSS-Protection: 0');
